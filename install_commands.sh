6cea2bac2ff68ed78f3a9008b8b54648de0bae97
1r0nx <judemartinbalaro@gmail.com>
2025-11-24 21:04:27 +0100

    update install_commands.sh

--- install_commands.sh
+++ install_commands.sh
@@ -13,6 +13,10 @@ echo "-----------> INSTALLING/CHECKING zsteg"
 sudo gem install zsteg
 
 ## Uncomment the following lines to set a python3-venv in ~/Documents/Python
-#sudo apt-get install python3 python3-venv
-#python3 -m venv ~/Documents/Python
-#pip3 install oletools
+#if [ -n "$VIRTUAL_ENV" ]; then
+#    echo "A python-venv is already set as: $VIRTUAL_ENV"
+#else
+#    sudo apt-get install python3 python3-venv
+#    python3 -m venv ~/Documents/Python
+#    pip3 install oletools
+#fi
