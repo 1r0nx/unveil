537f10fd06d31583bf0b5bc081ccc0f68961dcdc
1r0nx <judemartinbalaro@gmail.com>
2025-11-24 19:16:29 +0100

    update commands.sh

--- install_commands.sh
+++ install_commands.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-sudo apt-get install 7z binwalk checksec docs2txt exiftool fdisk ffmpeg file \
-nm objdump pdfimages pdfinfo pdftotext pngcheck soxi sqlite3 unzip xxd mmls
+sudo apt-get install binutils 7zip binwalk checksec docx2txt exiftool fdisk ffmpeg file \
+poppler-utils pngcheck sqlite3 unzip xxd sleuthkit 
+#libasound2-plugins libsox-fmt-all sox
 
 sudo gem install zsteg
